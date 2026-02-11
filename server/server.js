72ab0d6c1964ea04a202a29fd78f126ce2f2b5bd
CaiooFerreiaa <caioferreiraadev@gmail.com>
2026-02-10 22:52:37 -0300

    Fix: sintaxe catch-all para Express 5

--- server/server.js
+++ server/server.js
@@ -54,7 +54,7 @@ if (process.env.NODE_ENV === "production") {
   const clientPath = path.join(__dirname, "..", "client", "dist");
   app.use(express.static(clientPath));
 
-  app.get("*", (_req, res) => {
+  app.get("(.*)", (_req, res) => {
     res.sendFile(path.join(clientPath, "index.html"));
   });
 }
